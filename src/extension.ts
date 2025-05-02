db3a4192ce4ed61d4167fc83e8687493a74fdbd4
Andrew Holbrook <holz55@gmail.com>
2025-05-02 10:23:41 -0600

    refactor: extract `/src/tree`

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
-import { SuffixTreeDataProvider } from './SuffixTreeDataProvider';
+import { SuffixTreeDataProvider } from './tree/SuffixTreeDataProvider';
 
 export async function activate(context: vscode.ExtensionContext) {
   console.log('[Suffixes] Activating extension...');
